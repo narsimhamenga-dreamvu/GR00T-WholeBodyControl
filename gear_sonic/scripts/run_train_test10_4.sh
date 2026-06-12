8ecdc8638b2b87566683e4813f8f51868d2e6831
Narsimha Menga <narsimha.menga@dreamvu.ai>
2026-06-12 18:16:33 +0000

    made changes to mixed precisoin

--- gear_sonic/scripts/run_train_test10_4.sh
+++ gear_sonic/scripts/run_train_test10_4.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-accelerate launch --num_processes=8 gear_sonic/train_agent_trl.py \
+accelerate launch \
+  --num_processes=1 \
+  --mixed_precision=no \
+  --num_machines=1 \
+  --dynamo_backend=no \
+  gear_sonic/train_agent_trl.py \
   +exp=manager/universal_token/all_modes/sonic_release \
   +checkpoint=sonic_release/last.pt \
   num_envs=4096 \
   headless=True \
+  "++algo.trl.bf16=false" \
   "++manager_env.commands.motion.motion_lib_cfg.motion_file=gear_sonic/data/gmr_test10_4_motion_lib.pkl" \
   "++manager_env.commands.motion.motion_lib_cfg.smpl_motion_file=dummy" \
   "++manager_env.commands.motion.encoder_sample_probs.g1=1.0" \
