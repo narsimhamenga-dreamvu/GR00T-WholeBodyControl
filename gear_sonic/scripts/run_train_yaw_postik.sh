311e6e7c27324a1515334dd1d299f154814ec5e8
Narsimha Menga <narsimha.menga@dreamvu.ai>
2026-06-26 03:11:50 +0000

    added training scritps for yaw postik

---
+++ gear_sonic/scripts/run_train_yaw_postik.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+
+NUM_GPUS=${1:-8}
+
+accelerate launch \
+  --num_processes=${NUM_GPUS} \
+  --mixed_precision=no \
+  --num_machines=1 \
+  --dynamo_backend=no \
+  gear_sonic/train_agent_trl.py \
+  +exp=manager/universal_token/all_modes/sonic_release \
+  +checkpoint=sonic_release/last.pt \
+  num_envs=4096 \
+  headless=True \
+  "++algo.config.save_interval=100" \
+  "++algo.trl.bf16=false" \
+  "++experiment_dir=/data/fast/workspaces/user3/sonic_checkpoints/yaw_postik" \
+  "++manager_env.commands.motion.motion_lib_cfg.motion_file=gear_sonic/data/gmr_yaw_postik_motion_lib.pkl" \
+  "++manager_env.commands.motion.motion_lib_cfg.smpl_motion_file=dummy" \
+  "++manager_env.commands.motion.encoder_sample_probs.g1=1.0" \
+  "++manager_env.commands.motion.encoder_sample_probs.teleop=0.0" \
+  "++manager_env.commands.motion.encoder_sample_probs.smpl=0.0"
